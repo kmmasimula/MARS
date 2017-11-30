87f4879154624d7112f99991633b9884cf53ff3d
mfanamasimula <u12077713@tuks.co.za>
2017-11-30 12:04:31 +0200

    Adddition of databese models

--- django/SATwebsite/SAT/admin.py
+++ django/SATwebsite/SAT/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
-from .models import Team
+from .models import Team,User,MBTI,participatoryentity,bellbin,teamattributes,teamRank,teamQuality,teamHours,teamMBTI,teambellbin
 
 # Register your models here.
 
 admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(participatoryentity)
+admin.site.register(MBTI)
+admin.site.register(bellbin)
+admin.site.register(teamattributes)
+admin.site.register(teamRank)
+admin.site.register(teamQuality)
+admin.site.register(teamHours)
+admin.site.register(teamMBTI)
+admin.site.register(teambellbin)
--- django/SATwebsite/SAT/models.py
+++ django/SATwebsite/SAT/models.py
@@ -4,7 +4,99 @@ class Team(models.Model):
 	identifier =models.CharField(max_length=250)
 	teamNr =models.CharField(max_length=100)
 	teamName=models.CharField(max_length=250)
+	teamID =models.CharField(max_length=250)
+	name=models.CharField(max_length=250,default='')
+	year=models.CharField(max_length=4)
+	rond=models.CharField(max_length=100,default='0')
+	size=models.CharField(max_length=5,default='')
 
 
 	def __str__(self):
-		return self.identifier
+		return self.name
+
+class User(models.Model):
+	userID =models.CharField(max_length=250)
+	password=models.CharField(max_length=250)
+	title=models.CharField(max_length=250)
+	initials=models.CharField(max_length=250)
+	name=models.CharField(max_length=250)
+	surname=models.CharField(max_length=250)
+	cellphone=models.CharField(max_length=250)
+	emailaddress=models.CharField(max_length=250)
+	kind=models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.name
+
+
+class MBTI(models.Model):
+	userID =models.CharField(max_length=250)
+	Type=models.CharField(max_length=250)
+	
+	def __str__(self):
+		return self.userID
+
+class participatoryentity(models.Model):
+	"""docstring for Participatory"""
+	userID =models.CharField(max_length=250)
+	teamID=models.CharField(max_length=250)
+	Type =models.CharField(max_length=250)
+	value=models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.userID
+
+class bellbin(models.Model):
+	userID =models.CharField(max_length=250)
+	SH=models.CharField(max_length=10)
+	IM=models.CharField(max_length=10)
+	CF=models.CharField(max_length=10)
+	CO=models.CharField(max_length=10)
+	RI=models.CharField(max_length=10)
+	PL=models.CharField(max_length=10)
+	ME=models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.userID
+
+class teamattributes(models.Model):
+	teamID =models.CharField(max_length=250)
+	diversity =models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.teamID
+
+class teamRank(models.Model):
+	teamID =models.CharField(max_length=250)
+	rankmatrix =models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.teamID
+
+class teamQuality(models.Model):
+	teamID =models.CharField(max_length=250)
+	qualitymatrix =models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.teamID
+
+class teamHours(models.Model):
+	teamID =models.CharField(max_length=250)
+	hoursmatrix =models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.teamID
+
+class teamMBTI(models.Model):
+	teamID =models.CharField(max_length=250)
+	mbtimatrix =models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.teamID
+
+class teambellbin(models.Model):
+	teamID =models.CharField(max_length=250)
+	bellbinmatrix =models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.teamID
